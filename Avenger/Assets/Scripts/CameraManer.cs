977465d6565be14a226674d2837ca61ded5084db
Danil-Ivanenko <164200407+Danil-Ivanenko@users.noreply.github.com>
2024-06-24 00:06:38 +0700

    Mouse camer control added

--- Avenger/Assets/Scripts/CameraManer.cs
+++ Avenger/Assets/Scripts/CameraManer.cs
@@ -15,13 +15,27 @@ public class CameraManer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if(target == null)
+        //{
+        //    target = GameObject.FindGameObjectWithTag("Player").transform;
+        //}
+        //transform.position = new Vector3(target.position.x, target.position.y, -1);
+
         if(target == null)
         {
             target = GameObject.FindGameObjectWithTag("Player").transform;
         }
-        transform.position = new Vector3(target.position.x, target.position.y, -1);
 
 
+   
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+        Vector2 middlePoint = (target.position + (Vector3)cursorPosition) / 2;
+
+    
+        transform.position = new Vector3(middlePoint.x, middlePoint.y, -1);
+        
     }
     
 }
